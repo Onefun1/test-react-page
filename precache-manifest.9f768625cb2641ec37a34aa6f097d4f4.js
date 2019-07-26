f8b728b32e8db050fd15bc5fe2d4be4ed8c8bc35
MAKSYM_enko <finside41@gmail.com>
2019-07-26 06:16:53 +0300

    Updates

---
+++ precache-manifest.9f768625cb2641ec37a34aa6f097d4f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d40fe74f802f7b529229808186c6ea",
+    "url": "/test-react-page/index.html"
+  },
+  {
+    "revision": "7b5cf7416c96bd1e10a1",
+    "url": "/test-react-page/static/css/main.4ed6d0f5.chunk.css"
+  },
+  {
+    "revision": "70760e3a0cdbc0ea8a4a",
+    "url": "/test-react-page/static/js/2.4af0fa91.chunk.js"
+  },
+  {
+    "revision": "7b5cf7416c96bd1e10a1",
+    "url": "/test-react-page/static/js/main.ba35e5b2.chunk.js"
+  },
+  {
+    "revision": "60ed77ea4e85927d85f7",
+    "url": "/test-react-page/static/js/runtime~main.1bf273fe.js"
+  },
+  {
+    "revision": "7019ba5c99b7987404fd4a9c20f5e9a5",
+    "url": "/test-react-page/static/media/imac.7019ba5c.jpeg"
+  },
+  {
+    "revision": "fce98d93c1dfa5df1d651d218d02b9b2",
+    "url": "/test-react-page/static/media/star-active.fce98d93.svg"
+  },
+  {
+    "revision": "521e387638e5e2607a15df22a833ace1",
+    "url": "/test-react-page/static/media/star.521e3876.svg"
+  }
+]);
